a2f351c6527e391f9e811103df49aadd98a3983f
git <THEObuildbot>
2025-03-13 11:20:25 +0100

    Release THEOplayerGoogleCastIntegration version 8.13.0

--- Package.swift
+++ Package.swift
@@ -22,8 +22,8 @@ let package = Package(
     ),
     .binaryTarget(
       name: "THEOplayerGoogleCastIntegration",
-      url: "https://cdn.theoplayer.com/build/sdk-apple/8.12.1/THEOplayerGoogleCastIntegration.xcframework.zip",
-      checksum: "9b20928cf51d8e8bac926569d89ccbd80af666bf7a014ebc7d5b51d2841b1c7a"
+      url: "https://cdn.theoplayer.com/build/sdk-apple/8.13.0-/THEOplayerGoogleCastIntegration.xcframework.zip",
+      checksum: "c023702378808e43bf9d2da55620d46b9c8a437c97612ac1b96c24e4c32e80e4"
     ),
     .binaryTarget(
       name: "THEOplayerSDK",
